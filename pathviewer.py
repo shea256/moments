581c9561d1877ee95af0404e2876e12111ceafde
Ryan Shea <me@ryanshea.org>
2012-03-27 13:58:33 -0400

    making photos link to path pages and fixing ascii character bug

--- pathviewer.py
+++ pathviewer.py
@@ -79,7 +79,7 @@ class GetHtmlFromUrlThread(threading.Thread):
                             moment.setPhotoUrl(photo_url)
                             #print moment.getPhotoUrl()
                     except:
-                        "unable to parse html"
+                        print "unable to parse html"
                 except:
                     print "unable to read html"
             except:
@@ -117,6 +117,9 @@ def get_tweet_results_from_search_url(search_url):
     tweet_results = resultsContainer['results']
     return tweet_results
 
+def removeNonAscii(s):
+    return "".join(i for i in s if ord(i)<128)
+
 def get_photo_moments_from_tweet_results(tweet_results):
     moments = []
     for result in tweet_results:
@@ -129,6 +132,7 @@ def get_photo_moments_from_tweet_results(tweet_results):
                     moment_id = result['id']
                     path_url = words[wordcount-1]
                     path_url = path_url.rstrip('\"')
+                    path_url = removeNonAscii(path_url)
                     from_user = result['from_user']
                     moment = Moment(moment_id, text, path_url, from_user)
                     moments.append(moment)
@@ -151,7 +155,6 @@ def add_photo_links_to_photo_moments(moments):
     
     return moments
 
-
 def get_current_photo_moments():
     SEARCH_BASE = 'http://search.twitter.com/search.json?q=path.com%2Fp%2F&result_type=recent'
     tweet_results = get_tweet_results_from_search_url(SEARCH_BASE)
@@ -165,9 +168,10 @@ def get_current_photo_moments():
 def update_photo_moments_in_cache():
     print "updating cache..."
     photo_moments = get_current_photo_moments()
-    latest_photo_url = str(photo_moments[0].getPhotoUrl())
-    latest_photo_id = str(photo_moments[0].getId())
-    p['a_channel'].trigger('an_event', {'photo_url': latest_photo_url, 'id':latest_photo_id})
+    photo_url = str(photo_moments[0].getPhotoUrl())
+    photo_id = str(photo_moments[0].getId())
+    path_url = str(photo_moments[0].getPathUrl())
+    p['a_channel'].trigger('an_event', {'photo_url': photo_url, 'id':photo_id, 'path_url':path_url})
     cache.set('photo_moments', photo_moments, timeout=15)
 
 def get_photo_moments_from_cache():
@@ -197,5 +201,5 @@ if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-    #runRefresher()
+    runRefresher()
 
